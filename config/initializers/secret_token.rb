71d17da324aca740aa7b023a652986cb5b6b276e
root <root@RoR-ExtJS.(none)>
2013-04-26 10:27:56 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sampleapp::Application.config.secret_token = '7ea5126d9d49626a94a4c6660e02af8aef5d1a22fdff5c7bdaff3978c881fed9717f0d16974eae9c6c3dfd690bac6bfb9218039ce80c3ba774cd46abb0f3b5d3'
